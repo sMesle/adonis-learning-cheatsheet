f60e9cb5dbdf42d9b3187ef72915aff4b2e2d8ae
sMesle <steve.demeulemeester@efrei.net>
2020-11-01 19:07:21 +0100

    Added basics ressources * Customer * Projects * Tasks

---
+++ my-first-api/README.md
@@ -0,0 +1,9 @@
+# AdonisJS : My first api
+
+This first api is to understand basics concepts of AdonisJS
+
+## Models
+
+- Customers
+- Projects
+- Tasks
---
+++ my-first-api/app/Controllers/Http/ProjectsController.ts
@@ -0,0 +1,13 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ProjectsController {
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
